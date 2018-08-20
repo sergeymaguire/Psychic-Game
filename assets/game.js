bc92f0f013592780c6f6aa795f3eb8e4cd53517e
sergeymaguire <sergeymaguire@gmail.com>
2018-08-20 11:12:29 -0700

    yayaya my hw is done so ealry time to do the hangman gamegit add index.html

--- assets/game.js
+++ assets/game.js
@@ -3,6 +3,17 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 var guessesCounter = 0;
+var computerChoice = "";
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+setComputerLetter();
+
+function setComputerLetter() {
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+}
 
 function setWins(value) {
     document.getElementById("wins").innerText = value;
@@ -12,39 +23,76 @@ function setLosses(value) {
     document.getElementById("losses").innerText = value;
 }
 
-function setGuesses(value) {
-    document.getElementById("guesses").innerText = value;
+function setGuessesSoFar(value) {
+    document.getElementById("guessessofar").innerText = value;
 }
 
 function setGuessesLeft(value) {
     document.getElementById("guessesleft").innerText = value;
 }
 
-function setGuessesSoFar(value) {
-    guessesCounter++;
-    if (guessesCounter < 10) {
-        var s = document.getElementById("guesses").innerText;
-        s = s + value + ",";
-        document.getElementById("guesses").innerText = s;
+function winner() {
+    wins++;
+    setWins(wins);
+    guessesLeft = 9;
+    setGuessesSoFar("");
+    setComputerLetter();
+}
+
+function resetGame() {
+    guessesLeft = 9;
+    guessesCounter = 0;
+    setGuessesSoFar("");
+    gameLost();
+    setComputerLetter();
+}
+
+function setPageGuessesSoFar(key) {
+    setGuessesLeft(guessesLeft);
+    var s = document.getElementById("guessessofar").innerText;
+    s = s + key + ",";
+    document.getElementById("guessessofar").innerText = s;
+}
+
+function gameLost() {
+    setGuessesSoFar("");
+    guessesCounter = 0;
+    losses++;
+    setLosses(losses);
+    guessesLeft--;
+    setGuessesLeft(guessesLeft);
+
+}
+
+function applyKey(key) {
+    if (key === computerChoice) {
+        winner();
+        return;
     }
-    else {
-        setGuesses("");
-        guessesCounter = 0;
+
+    guessesCounter++;
+    guessesLeft--;
+
+    if (guessesCounter < 10) {
+        if (guessesCounter === 9) {
+            resetGame();
+        } else {
+            setPageGuessesSoFar(key);
+        }
     }
 }
 
 //Intialize Game Value
 setWins(0);
 setLosses(0);
-setGuesses("");
+setGuessesSoFar("");
 setGuessesLeft(9);
 
-
-document.addEventListener('keydown', function (event) {
-    if ((event.keyCode >= 97 && event.keyCode <= 122) ||
-        (event.keyCode >= 65 && event.keyCode <= 90)) {
+function handleKey(event) {
+    if (event.key >= "a" && event.key <= "z") {
         // alert('A letter was pressed');
-        setGuessesSoFar(event.key);
-
+        applyKey(event.key);
     }
-});
+};
+
+document.addEventListener('keydown', handleKey);
